0b8a225e2df77398f873ff7a571e3d2f126fc112
Nicolas Petillon <nicolas.petillon@dewizz.com>
2018-03-29 13:06:17 +0200

    draft cache module

--- libs/cache/src/cache.decorator.ts
+++ libs/cache/src/cache.decorator.ts
@@ -1,21 +1,38 @@
-import {Reflect} from 'reflect-metadata/Reflect';
-import { Cache } from './cache.model';
+import 'reflect-metadata/Reflect';
+import {Cache} from './cache.model';
 import {CACHES} from "./cache.manager";
 
-export const METADATA_KEY_CACHE_DEFAUTLS = '_cache_defaults';
+export const METADATA_KEY_CACHE_DEFAULTS = '_cache_defaults';
 export const METADATA_KEY_CACHE_KEYS = '_cache_keys';
 export const METADATA_KEY_CACHE_VALUE = '_cache_value';
 
 export function CacheDefaults(cacheName: string): ClassDecorator {
   return <TFunction extends Function>(target: TFunction): TFunction | void => {
-    Reflect.defineMetadata(METADATA_KEY_CACHE_DEFAUTLS, cacheName, target);
+    Reflect.defineMetadata(METADATA_KEY_CACHE_DEFAULTS, cacheName, target);
     return;
   }
 }
 
 export function CacheResult(cacheName: string = ''): MethodDecorator {
-  return <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> => {
-    return;
+  return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any> => {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      const cache: Cache<any, any> = getCache(target, cacheName);
+      const cacheKey: string = getCacheKey(target, propertyKey, args);
+
+      // Find cache value
+      let result: any = cache.get(cacheKey);
+
+      // Call function & save function if no cache value
+      if(result === undefined) {
+        // Call function & save result
+        result = originalMethod.apply(this, args);
+        cache.put(cacheKey, result);
+      }
+
+      return result;
+    };
+    return descriptor;
   }
 }
 
@@ -28,8 +45,20 @@ export function CacheKey(): ParameterDecorator {
 }
 
 export function CachePut(cacheName: string = ''): MethodDecorator {
-  return <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> => {
-    return;
+  return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any> => {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      const cache: Cache<any, any> = getCache(target, cacheName);
+      const cacheKey: string = getCacheKey(target, propertyKey, args);
+      const indexValue: number = Reflect.getMetadata(`${METADATA_KEY_CACHE_VALUE}_${propertyKey}`, target, propertyKey);
+
+      if(indexValue && indexValue >= 0 && indexValue < args.length) {
+        cache.put(cacheKey, args[indexValue]);
+      }
+
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
   }
 }
 
@@ -40,21 +69,14 @@ export function CacheValue(): ParameterDecorator {
 }
 
 export function CacheRemove(cacheName: string = ''): MethodDecorator {
-  return <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> => {
-    return;
-  }
-}
-
-export function CacheRemoveAll(cacheName: string = ''): MethodDecorator {
-  return <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> => {
+  return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any> => {
     const originalMethod = descriptor.value;
     descriptor.value = function (...args: any[]) {
-      const cache: Cache = getCache(target, cacheName);
-      const result: any = originalMethod.apply(this, args);
+      const cache: Cache<any, any> = getCache(target, cacheName);
+      const cacheKey: string = getCacheKey(target, propertyKey, args);
 
-      if (cache) {
-        cache.removeAll();
-      }
+      const result: any =  originalMethod.apply(this, args);
+      cache.remove(cacheKey);
 
       return result;
     };
@@ -62,13 +84,38 @@ export function CacheRemoveAll(cacheName: string = ''): MethodDecorator {
   }
 }
 
-export function getCache(target: Object, cacheName: string = ''): Cache {
-  if(cacheName === '') {
-    cacheName = Reflect.getMetadata(METADATA_KEY_CACHE_DEFAUTLS, target) || '';
+export function CacheRemoveAll(cacheName: string = ''): MethodDecorator {
+  return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any> => {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      const cache: Cache<any, any> = getCache(target, cacheName);
+      const result: any = originalMethod.apply(this, args);
+
+      cache.removeAll();
+
+      return result;
+    };
+    return descriptor;
   }
-  return CACHES.get(cacheName);
 }
 
-export function getCacheKey(): string {
-  Reflect.getMetadata(`${METADATA_KEY_CACHE_KEYS}_${propertyKey}`, target, propertyKey);
+export function getCache(target: Object, cacheName: string = ''): Cache<any, any> {
+  if (cacheName === '') {
+    cacheName = Reflect.getMetadata(METADATA_KEY_CACHE_DEFAULTS, target) || '';
+  }
+  const cache: Cache<any, any> = CACHES.get(cacheName);
+  if(!cache) {
+    throw new Error(`Cache '${cacheName}' not found`);
+  }
+
+  return cache;
+}
+
+export function getCacheKey(target: Object, propertyKey: string | symbol, ...args: any[]): string {
+  const indices: number[] = Reflect.getMetadata(`${METADATA_KEY_CACHE_KEYS}_${propertyKey}`, target, propertyKey);
+  if (indices) {
+    args = args.filter((value: any, index: number) => indices.indexOf(index) !== -1);
+  }
+
+  return args.map(a => (JSON.stringify(a) || a.toString())).join('|');
 }
--- libs/cache/src/cache.manager.ts
+++ libs/cache/src/cache.manager.ts
@@ -1,14 +1,14 @@
+import { Cache } from './cache.model';
+
 class CacheManager {
 
-  cache: Map<string, Cache>;
+  cache: Map<string, Cache<any, any>>;
 
   constructor() {
-    this.cache = new Map<string, Cache>();
+    this.cache = new Map<string, Cache<any, any>>();
   }
-
-
 
 }
 
 
-export const CACHES: Map<string, Cache> = new Map<string, Cache>();
+export const CACHES: Map<string, Cache<any, any>> = new Map<string, Cache<any, any>>();
