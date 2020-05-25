251e0d8a4e964aae2ec5addf806e141fd71aed07
Nicolas Petillon <nicolas.petillon@dewizz.com>
2020-05-25 21:18:33 +0200

    :bug: Queue decorator fix

--- projects/utils/src/lib/wait.decorator.ts
+++ projects/utils/src/lib/wait.decorator.ts
@@ -4,4 +4,6 @@ import {Queue} from './queue.decorator';
  * Wait for a Promise / Subscription before to be re-executed
  * /!\ the method result is modified => Return a Promise
  */
-export const Wait = (name?: string) => Queue.bind(this, 1, name);
+export function Wait(name?: string): MethodDecorator {
+  return Queue.bind(this, 1, name);
+}
